fe467d63ae69c8ef1209ef0d84d937a9b5816075
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-05-19 21:40:59 +0000

    Initial commit

---
+++ components/Nav.jsx
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+export default function Nav(){
+	return (
+		<>
+			<Head>
+			<title>Ἰερεμίας ePortfolio</title>
+			</Head>
+		<div className="navbar bg-base-300">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabindex="0" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52">
+        <li><a href="/">Home</a></li>
+        <li tabindex="0">
+          <a className="justify-between">
+            About
+            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+          </a>
+          <ul className="bg-base-300 p-2">
+            <li><a>Intrests</a></li>
+            <li><a>Fun Facts</a></li>
+          </ul>
+        </li>
+        <li><a>Goals</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost normal-case text-xl">ارميا ايهاب صموئيل</a>
+  </div>
+  <div className="navbar-center hidden lg:flex ">
+    <ul className="bg-base-300 menu menu-horizontal p-0">
+      <li><a>Home</a></li>
+      <li tabindex="0">
+        <a>
+          About
+          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+        </a>
+        <ul className="bg-base-300 p-2">
+          <li><a>Intrests</a></li>
+          <li><a>Fun Facts</a></li>
+        </ul>
+      </li>
+      <li><a>Goals</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn" href="mailto:jr.isamuel08.dev@outlook.com">Contact Me!</a>
+  </div>
+</div>
+		</>
+	)
+}
